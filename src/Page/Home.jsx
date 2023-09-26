85ec5b63effc0fbb1291442e4dd4e8af042d63b0
Kathy-kay <adigunkafy27@gmail.com>
2023-09-26 22:54:56 +0100

    added loader

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -2,15 +2,35 @@ import Header from "../Componenet/Header";
 import About from "../Componenet/About";
 import Project from "../Componenet/Project";
 import Contact from "../Componenet/Contact";
+import { useState, useEffect } from "react";
+import BarLoader from "../Componenet/BarLoader";
 
 const Home = () => {
+  const [Isloading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000); // Adjust the duration as needed
+  }, []);
+
   return (
-    <main className="min-h-screen bg-bay-300">
-      <Header />
-      <About />
-      <Project />
-      <Contact />
-    </main>
+    <>
+      {Isloading ? (
+        <div className="min-h-screen bg-bay-300 flex items-center justify-center">
+          {" "}
+          <BarLoader />
+        </div>
+      ) : (
+        <div className="min-h-screen bg-bay-300">
+          <Header />
+          <About />
+          <Project />
+          <Contact />
+        </div>
+      )}
+    </>
   );
 };
 
